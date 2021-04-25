d9121f75faf86a3746d02482807a2ab25312a648
ferblanco75 <ferblanco@gmail.com>
2021-04-24 21:51:58 -0300

    agrego endpoint apiReviews/id para buscar por id de pelicula

--- src/main/java/ar/edu/unq/desapp/grupoj/backenddesappapi/service/ReviewService.java
+++ src/main/java/ar/edu/unq/desapp/grupoj/backenddesappapi/service/ReviewService.java
@@ -1,12 +1,15 @@
 package ar.edu.unq.desapp.grupoj.backenddesappapi.service;
-
+import ar.edu.unq.desapp.grupoj.backenddesappapi.exception.ResourceNotFoundException;
 import ar.edu.unq.desapp.grupoj.backenddesappapi.model.*;
 import ar.edu.unq.desapp.grupoj.backenddesappapi.repository.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 
 
 @CrossOrigin
@@ -22,6 +25,7 @@ public class ReviewService {
 
     @EventListener
     public void appReady(ApplicationReadyEvent event) {
+        reviewRepo.save(new Review(1, new Source("Netflix"),"Maso, para un domingo zafa","pochoclera",3,true,"fernando.test@gmail.com","fer", new Location("Ecuador","Quito"),new Language("Spanish")));
         reviewRepo.save(new Review(1, new Source("Netflix"),"Muy mala pelicula","No la entendi",1,true,"alonso.em@gmail.com","quique", new Location("Argentina","Buenos Aires"),new Language("Spanish")));
         reviewRepo.save(new Review(2, new Source("Netflix"),"Excelente, me conmovio! jaaaa","Un plato",5,false,"alonso.em@gmail.com","rodolfo", new Location("Argentina","Buenos Aires"),new Language("Spanish")));
         reviewRepo.save(new Review(3, new Source("Netflix"),"Pectacular, alta peli pero muy larga!","Increibles efecto especiales",3,false,"userAnonimo@gmail.com","pepe", new Location("Argentina","Buenos Aires"),new Language("Spanish")));
@@ -34,6 +38,12 @@ public class ReviewService {
     public Iterable<Review> getAllReviews() {
         return reviewRepo.findAll();
     }
+
+    @GetMapping("/apiReviews/{idMovie}")
+    public Iterable <Review> getReviewPorId(@PathVariable(value = "idMovie") Integer idMovie) throws ResourceNotFoundException{
+        return reviewRepo.findAllByIdMovie(idMovie);
+    }
+
 
 
 }
--- src/test/java/ar/edu/unq/desapp/grupoj/backenddesappapi/BackendDesappApiApplicationTests.java
+++ src/test/java/ar/edu/unq/desapp/grupoj/backenddesappapi/BackendDesappApiApplicationTests.java
@@ -1,13 +1,20 @@
 package ar.edu.unq.desapp.grupoj.backenddesappapi;
-
+import ar.edu.unq.desapp.grupoj.backenddesappapi.model.Review;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class BackendDesappApiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void ratingSevenInReviewReturnsSeven() {
+		Review review = new Review("buenaza",7);
+		assertEquals(7, review.getRating());
 	}
+
+
 
 }
