b27a5b213460dbbd60485ef5a0cd406c948f76b1
ferblanco75 <ferblanco@gmail.com>
2021-05-09 22:44:06 -0300

    modifico frontUserService y clase

--- src/main/java/ar/edu/unq/desapp/grupoj/backenddesappapi/model/FrontUser.java
+++ src/main/java/ar/edu/unq/desapp/grupoj/backenddesappapi/model/FrontUser.java
@@ -1,11 +1,24 @@
 package ar.edu.unq.desapp.grupoj.backenddesappapi.model;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(
+        name = "frontusers",
+        uniqueConstraints = {@UniqueConstraint(columnNames = {"email","password"})}
+)
+
 public class FrontUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
     private String name;
     private String email;
     private String password;
 
-    public FrontUser(String name, String email, String password) {
+    public FrontUser(Integer id, String name, String email, String password) {
+        this.id = id;
         this.name = name;
         this.email = email;
         this.password = password;
--- src/main/java/ar/edu/unq/desapp/grupoj/backenddesappapi/service/FrontUserService.java
+++ src/main/java/ar/edu/unq/desapp/grupoj/backenddesappapi/service/FrontUserService.java
@@ -6,9 +6,13 @@ import ar.edu.unq.desapp.grupoj.backenddesappapi.exception.NonExistentSourceExce
 import ar.edu.unq.desapp.grupoj.backenddesappapi.model.*;
 import ar.edu.unq.desapp.grupoj.backenddesappapi.repository.FrontUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.transaction.Transactional;
 
+@CrossOrigin
+@RestController
 public class FrontUserService {
     @Autowired
     private FrontUserRepository frontUserRepo;
