6984bb2382603ad2aa3f8addae6f5747a90a656a
alonsoem <alonso.em@gmail.com>
2021-05-27 18:06:07 -0300

    Source, User and Decade Service Coverage 100%

--- src/test/java/ar/edu/unq/desapp/grupoj/backenddesappapi/BackendDesappApiApplicationTests.java
+++ src/test/java/ar/edu/unq/desapp/grupoj/backenddesappapi/BackendDesappApiApplicationTests.java
@@ -1,16 +1,24 @@
 package ar.edu.unq.desapp.grupoj.backenddesappapi;
+import ar.edu.unq.desapp.grupoj.backenddesappapi.exception.NonExistentLocationException;
+import ar.edu.unq.desapp.grupoj.backenddesappapi.exception.NonExistentSourceException;
 import ar.edu.unq.desapp.grupoj.backenddesappapi.model.*;
 import ar.edu.unq.desapp.grupoj.backenddesappapi.model.user.Critic;
 import ar.edu.unq.desapp.grupoj.backenddesappapi.model.user.User;
 import ar.edu.unq.desapp.grupoj.backenddesappapi.repository.LanguageRepository;
-import ar.edu.unq.desapp.grupoj.backenddesappapi.service.RateDTO;
-import ar.edu.unq.desapp.grupoj.backenddesappapi.service.UserDTO;
+import ar.edu.unq.desapp.grupoj.backenddesappapi.repository.UserRepository;
+import ar.edu.unq.desapp.grupoj.backenddesappapi.service.*;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 
@@ -18,6 +26,14 @@ class BackendDesappApiApplicationTests {
 
 	public LanguageRepository repository;
 
+	@Autowired
+	private UserService userSrvc;
+	@Autowired
+	private UserRepository userRepo;
+	@Autowired
+	private SourceService sourceSrvc;
+	@Autowired
+	private DecadeService decadeSrvc;
 
 	@Test
 	void ratingThreeInReviewReturnsThree() {
@@ -62,4 +78,69 @@ class BackendDesappApiApplicationTests {
 		assertEquals(123, rateDTO.reviewId);
 	}
 	//RateDTO(UserDTO user, Integer reviewId,RateType rateType)
+
+	@Test
+	void UserServiceTest1() throws NonExistentLocationException, NonExistentSourceException {
+		Source source = Mockito.mock(Source.class);
+		Location location = Mockito.mock(Location.class);
+		User user = new User(source,"testUser","test1",location);
+		User retrievedUser= userSrvc.getBySourceAndUserIdAndNickId(1,"testUser","test1",1);
+		assertEquals("testUser", retrievedUser.getUserId());
+		assertEquals("test1", retrievedUser.getUserNick());
+
+	}
+
+
+	@Test
+	void RetrieveOneUserFromService() throws NonExistentLocationException, NonExistentSourceException {
+
+		Source sourceRetrieved = sourceSrvc.getById(1).get();
+		assertEquals("Netflix", sourceRetrieved.getName());
+		assertEquals(1, sourceRetrieved.getId());
+	}
+
+	@Test
+	void RetrieveAllUsersFromService() throws NonExistentLocationException, NonExistentSourceException {
+
+		List<String> sources =new ArrayList<>();
+
+		Iterator<Source> itr = sourceSrvc.findAll().iterator();
+		while(itr.hasNext()) {
+			Source source = itr.next();
+			sources.add(source.getName());
+		}
+
+		assertEquals(5, sources.size());
+		assertTrue(sources.contains("Netflix"));
+		assertTrue(sources.contains("Disney+"));
+		assertTrue(sources.contains("Amazon Prime Video"));
+		assertTrue(sources.contains("Paramount"));
+		assertTrue(sources.contains("HBO Go"));
+
+	}
+
+	@Test
+	void RetrieveOneDecadeFromService() {
+		Decade decadeRetrieved = decadeSrvc.getById("D80").get();
+		assertEquals(1980, decadeRetrieved.getFrom());
+		assertEquals(1989, decadeRetrieved.getTo());
+	}
+
+	@Test
+	void RetrieveAllDecadeFromService() {
+
+		List<Decade> decades =new ArrayList<>();
+
+		Iterator<Decade> itr = decadeSrvc.findAll().iterator();
+		while(itr.hasNext()) {
+			Decade decade= itr.next();
+			decades.add(decade);
+		}
+
+		assertEquals(5, decades.size());
+
+
+	}
+
+
 }
