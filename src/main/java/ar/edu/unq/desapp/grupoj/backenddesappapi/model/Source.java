536df54e73e4623bd3c89ab00f30bcdbe63106f6
ferblanco75 <ferblanco@gmail.com>
2021-06-16 22:55:23 -0300

    agrego metodo consumeMessage en clase source

--- src/main/java/ar/edu/unq/desapp/grupoj/backenddesappapi/model/Source.java
+++ src/main/java/ar/edu/unq/desapp/grupoj/backenddesappapi/model/Source.java
@@ -1,11 +1,16 @@
 package ar.edu.unq.desapp.grupoj.backenddesappapi.model;
 
+import ar.edu.unq.desapp.grupoj.backenddesappapi.config.MessagingConfig;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 
 @Entity
+@Component
 public class Source {
 
     @Id
@@ -26,6 +31,11 @@ public class Source {
 
     public String getName(){
         return this.name;
+    }
+
+    @RabbitListener(queues = MessagingConfig.QUEUE)
+    public void consumeMessageFromQueue(ReviewNotification reviewNotification) {
+        System.out.println("Message recieved from queue : " + reviewNotification);
     }
 
 }
