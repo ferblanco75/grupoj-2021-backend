45b9e114589c722e7d66a9975f3eb79249d83037
ferblanco75 <ferblanco@gmail.com>
2021-06-29 23:13:18 -0300

    agrego archivo de configuracion de redis

---
+++ src/main/java/ar/edu/unq/desapp/grupoj/backenddesappapi/config/RedisConfig.java
@@ -0,0 +1,28 @@
+package ar.edu.unq.desapp.grupoj.backenddesappapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+
+@Configuration
+@ComponentScan("ar.edu.unq.desapp.grupoj.backenddesappapi")
+@EnableRedisRepositories(basePackages = "ar.edu.unq.desapp.grupoj.backenddesappapi.repository")
+public class RedisConfig {
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        final RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        template.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
+        return template;
+    }
+}
