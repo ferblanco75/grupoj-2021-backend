e0263354e71b07e11233b1545d4e02723acd72d8
alonsoem <alonso.em@gmail.com>
2021-07-01 01:49:26 -0300

    Fix para no llamar con ID de plataforma

--- src/main/java/ar/edu/unq/desapp/grupoj/backenddesappapi/service/StatisticsService.java
+++ src/main/java/ar/edu/unq/desapp/grupoj/backenddesappapi/service/StatisticsService.java
@@ -1,8 +1,11 @@
 package ar.edu.unq.desapp.grupoj.backenddesappapi.service;
 
+import ar.edu.unq.desapp.grupoj.backenddesappapi.model.FrontUser;
 import ar.edu.unq.desapp.grupoj.backenddesappapi.model.Statistics;
 import ar.edu.unq.desapp.grupoj.backenddesappapi.repository.EstatisticsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,8 +19,11 @@ public class StatisticsService {
     private EstatisticsRepository repo;
 
     @Transactional
-    public List<Statistics> getAllStatisticsByPlatform(Integer platformId) {
-        return repo.findAllByPlatformId(platformId);
+    public List<Statistics> getAllStatisticsByPlatform() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        FrontUser userDetails = (FrontUser) auth.getPrincipal();
+
+        return repo.findAllByPlatformId(userDetails.getPlatformId());
     }
 
     @Transactional
